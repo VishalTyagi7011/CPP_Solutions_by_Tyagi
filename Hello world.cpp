b8950172ff17ea54553db2ec1635c39da0f4852e
VISHAL TYAGI <101706996+VishalTyagi7011@users.noreply.github.com>
2024-05-05 21:46:20 +0530

    Hello world!
    Hey bro,
    It's is your first code , when you learn programming.
    
    Thanks

---
+++ Hello world.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : Vishal Tyagi
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+//	return 0;
+//}
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello Vishal\"s \n";
+	cout<<"hello Tyagi";
+	return 0;
+}
