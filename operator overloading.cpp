7d33396ba50c92c715d80a59d283d7e4fdfd168f
VISHAL TYAGI <101706996+VishalTyagi7011@users.noreply.github.com>
2024-08-28 21:48:32 +0530

    Update operator overloading.cpp

--- operator overloading.cpp
+++ operator overloading.cpp
@@ -1,24 +1,37 @@
-//============================================================================
-// Name        : operator.cpp
-// Author      : Vishal Tyagi
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
+// Online C++ compiler to run C++ program online
 #include <iostream>
-using namespace std;
-class Operators
+class Complex
 {
-public:
-	void operator+(Operators obj)
-	{
-		cout<<"operator overloading";
-	}
+  int real, image;
+  public:
+  //default consturor
+  Complex(){}
+  
+  //parameterised consturor
+  Complex(int real,int image):real(real),image(image) {}
+  
+  // fucntion
+  void display()
+  {
+      std::cout<<real<<"+ i"<<image;
+  }
+  
+  //operator overloading
+  Complex operator +(Complex &obj)
+  {
+      Complex ans;
+      ans.real=real+obj.real;
+      ans.image=image +obj.image;
+      return ans;
+      
+  }
 };
 
 int main() {
-  Operators obj1,obj2;
-  obj1+obj2;
-	return 0;
+    Complex obj1(3,4);
+    Complex obj2(5,6);
+    Complex obj3=obj1+obj2;
+    obj3.display();
+
+    return 0;
 }
